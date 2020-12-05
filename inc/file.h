30de01b7191c8500cea42cd9e02433ed8f85d93f
venkatasaiyamini <69252693+venkatasaiyamini@users.noreply.github.com>
2020-12-05 03:34:25 -0800

    Update file.h

--- inc/file.h
+++ inc/file.h
@@ -1,1 +1,39 @@
+#ifndef __CALCULATOR_OPERATION_H__
 
+#define __CALCULATOR_OPERATION_H__
+
+
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <stdio_ext.h>
+
+
+
+
+
+int add(int a, int b);
+
+
+
+
+
+int subtract(int a, int b);
+
+
+
+int multiply(int a, int b);
+
+
+
+
+
+
+
+int divide(int a, int b);
+
+
+
+#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
