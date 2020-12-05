a097b511410b8b65a39b161d12a13bb7ec132837
venkatasaiyamini <69252693+venkatasaiyamini@users.noreply.github.com>
2020-12-05 03:32:06 -0800

    Update file.c

--- srs/file.c
+++ srs/file.c
@@ -1,29 +1,47 @@
-#include <stdio.h>
-int main() {
-    char operator;
-    double first, second;
-    printf("Enter an operator (+, -, *,): ");
-    scanf("%c", &operator);
-    printf("Enter two operands: ");
-    scanf("%lf %lf", &first, &second);
+#include <calc.h>
 
-    switch (operator) {
-    case '+':
-        printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
-        break;
-    case '-':
-        printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
-        break;
-    case '*':
-        printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
-        break;
-    case '/':
-        printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
-        break;
-        // operator doesn't match any case constant
-    default:
-        printf("Error! operator is not correct");
-    }
 
-    return 0;
-  }
+
+int add(int a, int b)
+
+{
+
+    return a + b;
+
+}
+
+
+
+int subtract(int a, int b)
+
+{
+
+    return a- b;
+
+}
+
+
+
+int multiply(int a, int b)
+
+{
+
+    return a * b;
+
+}
+
+
+
+int divide(int a, int b)
+
+{
+
+    if(0 == b)
+
+        return 0;
+
+    else
+
+        return a/b;
+
+}
